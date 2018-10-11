9ed490228573b1afb2c61d5a726fbca253453fae
Nick Truesdell <39029300+kroople@users.noreply.github.com>
2018-10-10 20:41:26 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabW4_Truesdell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int dAmount1;
+            int dAmount2;
+            //decimal dAmount3;
+            int dResult;
+            //decimal dSum;
+            //decimal dDifference;
+
+
+            Console.WriteLine("Enter a amount for a first number:");
+            dAmount1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Now, enter a second amount to add:");
+            dAmount2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Now enter a third number:");
+            //dAmount3 = decimal.Parse(Console.ReadLine());
+
+            dResult = dAmount1 + dAmount2;
+            Console.WriteLine("The result of {0:C} + {1:C} = {2:C}", dAmount1, dAmount2, dResult);
+            
+
+            int x;
+            int y;
+            int z;
+            int xx;
+
+
+            x = 1;
+            y = 2;
+            z = 3;
+
+            xx=(x + y) * (z + 10);
+            Console.WriteLine(xx);
+            */
+
+            Console.WriteLine("Hello    World!");
+            Console.WriteLine("Hello ");
+            Console.WriteLine("World");
+            Console.WriteLine("\"Hello World!\"");
+            Console.WriteLine("Hello\\World!");
+
+
+
+        }
+
+    }
+}
