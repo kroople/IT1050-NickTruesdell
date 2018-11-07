2e2ddf31f10f067e1dbde6e261cf9bf99f872368
Nick Truesdell <39029300+kroople@users.noreply.github.com>
2018-11-07 15:11:22 -0600

    Add files via upload

---
+++ LabW8_Truesdell.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabW8
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {/*                 //1. <<<<<<<<<
+            int i = 1;
+            while (i < 11)
+            {
+                Console.WriteLine(i);
+
+                i = i + 2;
+            }
+            
+            //2.  <<<<<<<<<<<<<<<<<<<<<<<<<
+
+            int i = 30;
+            while (i < 46)
+            {
+                Console.WriteLine(i);
+
+                i = i +1;
+            }
+
+            for (i = 30; )
+            {
+                Console.WriteLine("Even"); //csrew it i cant figure this one out...
+            }
+            
+
+            //3. <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+            int number;
+
+            Console.WriteLine("Enter a number for military time:");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number <= 11)
+            {
+                Console.WriteLine("Good Morning");
+            }
+
+            else if (number <= 16; number > 12)
+            {
+                Console.WriteLine("Good Afternoon");
+            }
+
+            else if (number > 16)
+            {
+                Console.WriteLine("Good Evening");
+            }
+
+            
+
+            //4.<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+            int i = 10;
+            while (i < 21)
+            {
+                Console.WriteLine(i);
+            }
+            // It's just gonna keep printing 10, because it says while i is less than 21, print out 10. Here's what you do to fix it.
+
+            int i = 10;
+            while (i < 21)
+            {
+                Console.WriteLine(i);
+                i++; //<< this right here will add one to 10 until it hits 20, and since it is less than 20 it will stop and won't go over 21
+            }
+            
+
+            //5. <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+            for (int i = 0; i < 101; i++)
+                Console.WriteLine(i);
+            
+            Console.WriteLine("********");
+            */
+
+            //6. <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+        }
+
+
+
+
+    }
+}
