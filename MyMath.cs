bce08a7e38f45ac63fe3f00c679d12b495e15e11
Nick Truesdell <39029300+kroople@users.noreply.github.com>
2018-11-21 11:06:59 -0600

    Add files via upload

---
+++ MyMath.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabW10
+{
+    class MyMath
+    {
+        public double result;
+        public double operand1;
+        public double operand2;
+
+        public void Multiply(double operand1, double operand2)
+        {
+           
+        }
+        public void Divide(double operand1, double operand2)
+        {
+
+        }
+        public void Subtract(double operand1, double operand2)
+        {
+
+        }
+        public void Add(double operand1, double operand2)
+        {
+
+        }
+        public double GetResult()
+        {
+            return this.result;
+        }
+
+
+    }
+}
