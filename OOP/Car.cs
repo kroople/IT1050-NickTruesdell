bb93c8f6ccf560b0c70ac0f4c55b13b388aa482f
Nick Truesdell <39029300+kroople@users.noreply.github.com>
2018-12-12 14:12:13 -0600

    Add files via upload

---
+++ OOP/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Car
+    {
+        private string Make;
+        private string Model;
+        private int Year;
+        private int Speed;
+
+    }
+    public Car(string make, string model, int year, int speed)
+    {
+        this.Make = make;
+        this.Model = model;
+        this.Year = year;
+        this.Speed = speed;
+    }
+
+    public Car(string make, string model, int year)
+    {
+        this.Make = make;
+        this.Model = model;
+        this.Year = year;
+        
+    }
+    
+
+    public void SpeedUp()
+    {
+        this.Speed++;
+    }
+
+    public void SlowDown()
+    {
+        this.Speed - 10; 
+        
+        // I cannot figure this out!!!! I also do not get why I am getting errors for all of my properties and methods...
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(Year + " " + Make + " " + Model + " is going " + Speed + " MPH.");
+    }
+}
